6967758eb0e442467b2c35daa3d6776676982e2a
denzelwash <denzelwash33@mail.ru>
2020-05-11 15:40:11 +0300

    initial

---
+++ webpack.config.js
@@ -0,0 +1,80 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const {
+  CleanWebpackPlugin
+} = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const devMode = process.env.NODE_ENV === 'development';
+console.log(devMode);
+
+const jsLoaders = () => {
+  const loaders = [{
+    loader: 'babel-loader',
+    options: {
+      presets: ['@babel/preset-env']
+    }
+  }];
+
+  if (devMode) loaders.push('eslint-loader');
+
+  return loaders;
+};
+
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  mode: 'development',
+  entry: ['@babel/polyfill', './index.js'],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: devMode ? 'bundle.js' : 'bundle.[hash].js'
+  },
+  resolve: {
+    extensions: ['.js'],
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@core': path.resolve(__dirname, 'src/core')
+    }
+  },
+  devtool: devMode ? 'source-map' : '',
+  devServer: {
+    contentBase: './dist',
+    hot: devMode
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      filename: 'index.html',
+      minify: !devMode
+    }),
+    new CleanWebpackPlugin(),
+    new CopyPlugin([{
+      from: path.resolve(__dirname, 'src/favicon.ico'),
+      to: path.resolve(__dirname, 'dist')
+    }]),
+    new MiniCssExtractPlugin({
+      filename: devMode ? 'style.css' : 'style.[hash].css',
+    })
+  ],
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: jsLoaders()
+    },
+    {
+      test: /\.s[ac]ss$/i,
+      use: [{
+        loader: MiniCssExtractPlugin.loader,
+        options: {
+          hmr: devMode,
+          reloadAll: true
+        }
+      },
+      'css-loader',
+      'sass-loader',
+      ],
+    }],
+  }
+};
